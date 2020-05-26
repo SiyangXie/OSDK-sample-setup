76986d979806b2a8ee7bf2786cd0f670e0306d8f
SiyangXie <siyangxie@outlook.com>
2020-05-26 16:32:17 +0800

    add osdk-core to *.zip download path

--- download.sh
+++ download.sh
@@ -1,6 +1,7 @@
 # git clone OSDK v3.9
 git clone -b 3.9 https://github.com/dji-sdk/Onboard-SDK.git
 # Download Onboard-SDK-Resources-advanced-sensing-2.0.3-armv8.zip
+cd ./Onboard-SDK-3.9/osdk-core/
 curl https://github.com/dji-sdk/Onboard-SDK-Resources/raw/advanced-sensing-2.0.3-armv8/advanced-sensing-2.0.3.tar.gz
 # Download Onboard-SDK-Resources-waypointv2-core-1.0.0-armv8.zip
 curl https://github.com/dji-sdk/Onboard-SDK-Resources/raw/waypointv2-core-1.0.0-armv8/waypointv2-core-1.0.0.tar.gz
