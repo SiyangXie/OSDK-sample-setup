d23093e57c03d03840ba1f71c935f27a51fa578f
SiyangXie <siyangxie@outlook.com>
2020-05-26 16:28:21 +0800

    add git clone -b 3.9

--- download.sh
+++ download.sh
@@ -1,3 +1,5 @@
+# git clone OSDK v3.9
+git clone -b 3.9 https://github.com/dji-sdk/Onboard-SDK.git
 # Download Onboard-SDK-Resources-advanced-sensing-2.0.3-armv8.zip
 curl https://github.com/dji-sdk/Onboard-SDK-Resources/raw/advanced-sensing-2.0.3-armv8/advanced-sensing-2.0.3.tar.gz
 # Download Onboard-SDK-Resources-waypointv2-core-1.0.0-armv8.zip
